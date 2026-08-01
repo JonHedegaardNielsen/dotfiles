f60b178f3a98e1e157f23d35b82a43772854e6d9
Jon Nielsen <jon230706@proton.me>
2026-08-02 01:10:37 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo pacman -Syu hyprland neovim chromium rg fd ghostty kitty mako hyprpaper 
+hypridle hyprlock xdg-desktop-portal-hyprland fish npm nwg-look nwg-displays stow dotnet-sdk-10.0
+bat fzf eza wl-clipboard spotify-launcher waybar rofi pavucontrol nautilus btop yazi starship 
+pipewire wireplumber hyprpolkitagent qt6-wayland qt5-wayland hyprpicker 
